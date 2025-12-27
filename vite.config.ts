c7e7f5068cff91aa3c6cfa31a1cb2724afd11792
Tanmay Tekale <75844090+tanmaytekale@users.noreply.github.com>
2025-12-28 00:00:23 +0530

    Change base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tanmay-site-new/",
+  base: "/radical-creativity.github.io/",
 })
