42919af798a8fbc821379668ec3c749f16c5c88a
Tanmay Tekale <75844090+tanmaytekale@users.noreply.github.com>
2025-12-28 00:09:35 +0530

    same

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/radical-creativity.github.io/", // This prefix makes assets load correctly
+  base: "/radical-creativity.github.io/", // THIS IS THE FIX
 })
