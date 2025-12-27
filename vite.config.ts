0e3606be15dd4dbf6de980fa67d37cead6eee3e8
Tanmay Tekale <75844090+tanmaytekale@users.noreply.github.com>
2025-12-28 00:03:19 +0530

    new

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/radical-creativity.github.io/",
+  // This must match your repository name exactly
+  base: "/radical-creativity.github.io/", 
 })
